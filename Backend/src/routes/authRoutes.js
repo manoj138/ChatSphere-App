37d2a697b91f4ff85a638a1de298fad26fee4e44
Manoj Chougule <126501363+manoj138@users.noreply.github.com>
2026-04-26 18:49:41 +0530

    feat: implement Arcjet security middleware for rate limiting and bot protection on auth routes

--- Backend/src/routes/authRoutes.js
+++ Backend/src/routes/authRoutes.js
@@ -1,12 +1,15 @@
 const router = require("express").Router();
 
 const authController = require("../controllers/authController");
+const arcjetProtection = require("../moddleware/arcjetMiddleware");
 const protectRoute = require("../moddleware/authMiddleware");
 
+router.use(arcjetProtection);
 router.post("/signup",authController.signup)
 router.post("/login",authController.login)
 router.post("/logout",authController.logout)
 
 router.put("/update-profile", protectRoute, authController.updateProfile)
+
 
 module.exports = router;
