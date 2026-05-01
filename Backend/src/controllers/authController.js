04cb8dc99cf1990773787ac1c282fb68c63a797a
Manoj Chougule <126501363+manoj138@users.noreply.github.com>
2026-05-01 22:23:19 +0530

    feat: implement authentication controllers for user signup, login, and logout functionality

--- Backend/src/controllers/authController.js
+++ Backend/src/controllers/authController.js
@@ -1,13 +1,13 @@
 const User = require("../models/userModel");
-
-const {generateToken} = require("../lib/utils");
+const { generateToken } = require("../lib/utils");
+const bcrypt = require("bcryptjs");
 const { formatMongoError, handle422 } = require("../helper/errorHandler");
-const { handle201 } = require("../helper/successHandler");
+const { handle201, handle200 } = require("../helper/successHandler");
 
-const signup = async(req,res)=>{
-    const {username, email, password} = req.body;
+const signup = async (req, res) => {
+    const { username, email, password } = req.body;
     try {
-        if(!username ||!email || !password){
+        if (!username || !email || !password) {
             return handle422(res, "All fields are required");
         }
         const newUser = await User.create({
@@ -23,3 +23,43 @@ const signup = async(req,res)=>{
         formatMongoError(res, error);
     }
 }
+
+
+const login = async (req, res) => {
+    const { email, password } = req.body;
+    try {
+        if (!email || !password) {
+            return handle422(res, "All field are required")
+        }
+
+        const user = await User.findOne({ email });
+
+        if (!user) {
+            return handle422(res, { email: "invalid credentials" })
+        }
+
+        const isPasswordCorrect = await bcrypt.compare(password, user.password);
+
+        if (!isPasswordCorrect) {
+            return handle422(res, { password: "invalid credentials" })
+        }
+
+        generateToken(user._id, res);
+
+        handle200(res, "User logged in successfully")
+    } catch (error) {
+        console.log("Error in login controller: ", error)
+        formatMongoError(res, error);
+    }
+}
+
+const logout = async (req, res) => {
+    try {
+        res.cookie("jwt", "", { maxAge: 0 })
+        handle200(res, "User logged out successfully")
+    } catch (error) {
+        formatMongoError(res, error)
+    }
+}
+
+module.exports = { signup, login, logout }
